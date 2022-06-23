c9474195e794a6c310ee282afd1bff271f22c82f
Ethan Swafford <ethanswafford@yahoo.com>
2022-06-23 00:38:08 -0400

    devPush

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result')
+const startPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+console.log(squares)
+
+let currentIndex = 76
+
+function moveFrog() {
+
+    switch (e.key) {
+        case 'ArrowLeft':
+            console.log('move left')
+
+    }
+
+    squares[currentIndex].classList.add('frog')
+
+}
+
+document.addEventListener('keyup', moveFrog)
