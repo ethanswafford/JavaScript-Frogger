d3148449c223a0da2d334e45ecee39ce26f3245c
Ethan Swafford <ethanswafford@yahoo.com>
2022-06-27 03:44:44 -0500

    inDevPush

---
+++ utilities.js
@@ -0,0 +1,14 @@
+function animate() {
+    ctx3.clearRect(0, 0, canvas.width, canvas.height);
+    frogger.draw();
+    frogger.update();
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+// event listeners
+
+window.addEventListener('keydown', function (e) {
+
+});
