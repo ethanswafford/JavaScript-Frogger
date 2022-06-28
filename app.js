083bac2c007a8d0de7783dff2927726c8f2b76c4
Ethan Swafford <ethanswafford@yahoo.com>
2022-06-28 11:51:52 -0500

    inDevPush

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const canvas = document.getElementById('canvas1');
+const canvasOne = document.getElementById('canvas1');
 const ctx1 = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
-const canvas = document.getElementById('canvas2');
+const canvasTwo = document.getElementById('canvas2');
 const ctx2 = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
-const canvas = document.getElementById('canvas3');
+const canvasThree = document.getElementById('canvas3');
 const ctx3 = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
-const canvas = document.getElementById('canvas4');
+const canvasFour = document.getElementById('canvas4');
 const ctx4 = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
 
-const canvas = document.getElementById('canvas5');
+const canvasFive = document.getElementById('canvas5');
 const ctx5 = canvas.getContext('2d');
 canvas.width = 600;
 canvas.height = 600;
